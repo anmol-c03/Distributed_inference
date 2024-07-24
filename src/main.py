d6cb267a6d103fec0b9e2719469c37aaf21fe523
Anmol chalise <anmolc650@gmail.com>
2024-07-24 21:51:35 +0545

    cleaning the code

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ import model_top
 
 model_base=GPT2LMHeadModel.from_pretrained('gpt2')
 sd_base=model_base.state_dict()
-# import sys;sys.exit(0)
-
 
 def get_layers_weights(layer):
     return model_base.transformer.h[layer].state_dict()
@@ -27,17 +25,10 @@ import sys;sys.exit(0)
 '''
 
 
-# sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
-# print('\n\n')
-
-# print(model_hf.keys())
-# import sys;sys.exit(0)
 
 iter=model_base.config.n_layer
 for i in range(iter):
     model=model_top.GPT(model_top.GPTConfig(),i,iter)
-    # print(model.state_dict().keys())
-    # break
     sd=model.state_dict()
     sd_keys=sd.keys()
     list_sd_keys=list(sd_keys)
@@ -56,7 +47,6 @@ for i in range(iter):
 
     # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
     # this means that we have to transpose these weights when we import them
-    # assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
     if i==0:
         token,pos='transformer.wte.weight','transformer.wpe.weight'
         sd[token].copy_(sd_base[token])
@@ -85,12 +75,18 @@ for i in range(iter):
 
     models[i]=model
 
-# print(models[11].state_dict().keys())
-# import sys;sys.exit(0)
-# for i in range(iter):
-#     print(f'Model {i}:')
-#     print(models[i].state_dict().keys())
-#     print('\n')
+'''
+if one is interested in observing the models please uncomment the following code
+
+
+print(models[11].state_dict().keys())
+import sys;sys.exit(0)
+for i in range(iter):
+    print(f'Model {i}:')
+    print(models[i].state_dict().keys())
+    print('\n')
+'''
+
 
 
 import tiktoken
